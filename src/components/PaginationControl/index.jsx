faf796b75226da452e56a0b0449086dd08e72834
Matheus Dantas <correia.dantas@outlook.com>
2021-05-30 19:38:53 -0300

    feat: adicionando logo personalizado

--- src/components/PaginationControl/index.jsx
+++ src/components/PaginationControl/index.jsx
@@ -35,6 +35,7 @@ const PaginationControl = () => {
 
   useEffect(() => {
     handleNextPage(page);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page, search]);
 
   return (
